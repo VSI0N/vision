84bb7c1154605131ece51d8b980d11c21c61e0c9
Krishna Moni Das <das694272@gmail.com>
2023-09-06 20:24:40 +0530

    added vision page

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ def home():
   return render_template('home.html')
 
 
+
+@app.route("/V15l0N")
+def vision():
+  return render_template('vision.html')
+
+
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
