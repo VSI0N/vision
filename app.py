7642c3a0ec035d047ed69e85c48570a769a20fa3
VSI0N <144059935+VSI0N@users.noreply.github.com>
2023-09-06 14:39:37 +0530

    Created main python app file

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+def login_page():
+  return render_template('home_page.html')
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
